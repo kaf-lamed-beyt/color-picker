2ec0e7b65fa6e80535aa1a3e81efb83571694911
kaf-lamed-beyt <belac335@gmail.com>
2023-03-24 16:23:56 +0100

    feat: initial UI

--- index.js
+++ index.js
@@ -3,87 +3,83 @@ function createColorModal() {
   container.id = "color-modal";
 
   container.style = `
-    position: fixed;
-    z-index: 20;
-    top: 0;
-    right: 0;
-    width: 300px;
-    height: 100%;
-    background: var(--white);
-    boxShadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    overflowY: 'scroll',
-  `;
+      position: fixed;
+      z-index: 20;
+      top: 10px;
+      right: 20px;
+      width: 300px;
+      height: 300px;
+      border-radius: 4px;
+      background: #161b22;
+      boxShadow: 0 0 10px rgba(0, 0, 0, 0.5);
+      overflow-y: hidden;
+      padding: 20px 0px;
+    `;
 
   document.body.appendChild(container);
 }
 
 function getColors() {
-  const elements = document.querySelectorAll("*");
+  const elements = document.getElementsByTagName("*");
+  const colorArray = [];
 
-  // instead of an array, I'm using a Set here.
-  // best practices for DSA of some sort
-  const colorSet = new Set();
-
-  for (let i = 0; i >= elements.length; i++) {
+  for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
 
-    const color = window.getComputedStyle(element).getPropertyValue("color");
-    if (colorSet.has(color)) continue;
-    colorSet.add(color);
-
-    const colorTokenContainer = document.createElement("div");
-    colorTokenContainer.id = "token-container";
-    colorTokenContainer.style = `
-        display: 'flex',
-      `;
-
-    document.querySelector("#color-modal").appendChild(colorTokenContainer);
-
-    const colorToken = document.createElement("span");
-    // colorToken.id = "color-token";
-
-    colorToken.style = `
-        height: 20px;
-        width: 20px;
-        background: ${color};
-        margin: 0 10px 0 0; 
-      `;
-
-    const colorHex = document.createElement("p");
-    colorHex.textContent = color;
-
-    colorHex.style = `
-        width: 100%;
-        border: 1px solid red;
-      `;
-
-    document.querySelector("token-container").appendChild(colorToken);
-    document.querySelector("token-container").appendChild(colorHex);
+    const color =
+      window.getComputedStyle(element).getPropertyValue("color") &&
+      window.getComputedStyle(element).getPropertyValue("background") &&
+      window.getComputedStyle(element).getPropertyValue("background-color");
+    if (colorArray.includes(color)) continue;
+    colorArray.push(color);
   }
+
+  return colorArray;
 }
 
 createColorModal();
+const colors = getColors();
 
-document.addEventListener("DOMContentLoaded", function () {
-  var button = document.querySelector("#get-colors");
-  var modal = document.querySelector("#result");
-  var closeButton = document.querySelector("#close");
+colors.map((item, index) => {
+  const colorElement = document.createElement("div");
+  colorElement.id = "color-element";
+  colorElement.style = `
+      display: flex;
+    `;
 
-  button.addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "getColors" });
-    });
-    modal.style.display = "block";
-  });
+  const colorCode = document.createElement("p");
+  colorCode.id = "color-code";
+  colorCode.textContent = item;
 
-  closeButton.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
+  colorCode.style = `
+    width: 200px;
+    margin-left: 25px;
+    border: 1px solid blue;
+  `;
+
+  const colorToken = document.createElement("span");
+  colorToken.style = `
+      height: 18px;
+      width: 18px;
+      margin: 0 0 0 15px;
+      background: ${item};
+    `;
+
+  return document
+    .querySelector("#color-modal")
+    .appendChild(colorElement)
+    .appendChild(colorToken)
+    .appendChild(colorCode);
 });
 
-// access chrome's
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "getColors") {
-    getColors();
-  }
+var button = document.getElementById("get-colors");
+var modal = document.getElementById("color-modal");
+var closeButton = document.getElementById("close");
+
+button.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+
+closeButton.addEventListener("click", function () {
+  modal.style.display = "none";
 });
