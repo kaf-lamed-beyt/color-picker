9edbafe42417c8fe0b8056387324dcfd75afc41f
kaf-lamed-beyt <belac335@gmail.com>
2023-03-26 23:28:12 +0100

    minor

--- background.js
+++ background.js
@@ -2,15 +2,48 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log("Background script is running omo ope");
 });
 
-getColorsScript = (tab) => {
+// getColorsScript = (tab) => {
+//   const { id, url } = tab;
+
+//   chrome.scripting.executeScript({
+//     target: { tabId: id, allFrames: true },
+//     files: ["index.js"],
+//   });
+
+//   console.log(`loading: ${url}`);
+// };
+
+getColorsScript = async (tab) => {
   const { id, url } = tab;
+  const [tab_] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: id, allFrames: true },
-    files: ["index.js"],
-  });
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab_.id, allFrames: true },
+      func: () => {
+        const elements = document.getElementsByTagName("*");
+        const colorArray = [];
 
-  console.log(`loading: ${url}`);
+        for (let i = 0; i < elements.length; i++) {
+          const element = elements[i];
+          const computedStyle = window.getComputedStyle(element);
+
+          const color =
+            computedStyle.getPropertyValue("color") ||
+            computedStyle.getPropertyValue("background") ||
+            computedStyle.getPropertyValue("background-color");
+
+          if (!color || colorArray.includes(color)) continue;
+          colorArray.push(color);
+        }
+
+        return colorArray;
+      },
+    },
+    (result) => {
+      console.log(result);
+    }
+  );
 };
 
 getCurrentTab = async () => {
--- index.js
+++ index.js
@@ -2,17 +2,6 @@ const button = document.querySelector("#get-colors");
 const modal = document.querySelector(".color-modal");
 const closeButton = document.querySelector("#close");
 
-function createColorModal() {
-  const modal = `
-    <div class="color-modal">
-      <div class="color-modal-content"></div>
-      <button id="close">Close</button>
-    </div>
-  `;
-
-  document.body.insertAdjacentHTML("beforeend", modal);
-}
-
 function getColors() {
   const elements = document.getElementsByTagName("*");
   const colorArray = [];
@@ -22,13 +11,11 @@ function getColors() {
     const computedStyle = window.getComputedStyle(element);
 
     const color =
-      computedStyle.getPropertyValue("background-color") +
-      "," +
-      computedStyle.getPropertyValue("color") +
-      "," +
-      computedStyle.getPropertyValue("background");
+      computedStyle.getPropertyValue("background-color") ||
+      computedStyle.getPropertyValue("background") ||
+      computedStyle.getPropertyValue("color");
 
-    if (!color || colorArray.includes(color)) continue;
+    if (colorArray.includes(color)) continue;
     colorArray.push(color);
   }
 
@@ -36,11 +23,9 @@ function getColors() {
 }
 
 function renderColors(colors) {
-  const modalContent = document.querySelector(".color-modal-content");
+  const modalContent = document.querySelector(".color-modal");
 
   colors.forEach((item, index) => {
-    console.log(item);
-
     return (modalContent.innerHTML += `
       <div class="color-element" key=${index}>
         <span class="token" style="--bg: ${item}"></span>
@@ -50,15 +35,21 @@ function renderColors(colors) {
   });
 }
 
-createColorModal();
+const colors = getColors();
+renderColors(colors);
+
+if (modal) {
+  document.body.removeChild(button);
+}
 
 button.addEventListener("click", function () {
-  const colors = getColor();
-  renderColors(colors);
-
-  modal.style.display = "block";
+  document.body.appendChild(modal);
+  document.body.removeChild(button);
+  document.body.appendChild(closeButton);
 });
 
 closeButton.addEventListener("click", function () {
-  modal.style.display = "none";
+  document.body.removeChild(modal);
+  document.body.removeChild(closeButton);
+  document.body.appendChild(button);
 });
