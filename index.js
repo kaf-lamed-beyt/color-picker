de6d72ffc4366961baf82fbc9e60d4076f2868c8
kaf-lamed-beyt <belac335@gmail.com>
2023-03-28 14:10:40 +0100

    refactor app

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const modal = document.querySelector(".color-modal");
-const closeButton = document.querySelector("#close");
+const modal = document.createElement("div");
+modal.classList.add("color-modal");
+
+const closeButton = document.createElement("button");
+closeButton.classList.add("close");
+closeButton.innerText = "X";
 
 function renderColors(colors) {
   colors.forEach((item, index) => {
-    return (modal.innerHTML += `
-      <div class="color-element" key=${index}>
-        <span class="token" style="--bg: ${item}"></span>
-        <p class="color-code">${item}</p>
-      </div>
-      `);
+    const colorElement = document.createElement("div");
+    colorElement.classList.add("color-element");
+    colorElement.setAttribute("key", index);
+
+    const tokenElement = document.createElement("span");
+    tokenElement.classList.add("token");
+    tokenElement.style.setProperty("--bg", item);
+    colorElement.appendChild(tokenElement);
+
+    const codeElement = document.createElement("p");
+    codeElement.classList.add("color-code");
+    codeElement.textContent = item;
+    colorElement.appendChild(codeElement);
+
+    modal.appendChild(colorElement);
   });
+
+  document.body.insertAdjacentElement("beforeend", modal);
+  document.body.insertAdjacentElement("beforeend", closeButton);
 }
 
 function getColors() {
@@ -20,7 +36,10 @@ function getColors() {
     const element = elements[i];
     const computedStyle = window.getComputedStyle(element);
 
-    const color = computedStyle.getPropertyValue("background");
+    const color =
+      computedStyle.getPropertyValue("color") &&
+      computedStyle.getPropertyValue("background") &&
+      computedStyle.getPropertyValue("background-color");
 
     if (colorArray.includes(color)) continue;
     colorArray.push(color);
@@ -29,17 +48,10 @@ function getColors() {
   return colorArray;
 }
 
-modal.style.color = "#fff";
-
-const colors = getColors();
-
-modal.textContent = colors;
-
-renderColors(colors);
-
 closeButton.addEventListener("click", function () {
   document.body.removeChild(modal);
   document.body.removeChild(closeButton);
 });
 
-document.body.style.backgroundColor = "orange";
+const colors = getColors();
+renderColors(colors);
--- popup.js
+++ popup.js
@@ -1,31 +1,8 @@
-// chrome.runtime.onInstalled.addListener(() => {
-//   console.log("Background script is running omo ope");
-// });
-
-// getColorsScript = (tab) => {
-//   const { id, url } = tab;
-
-//   chrome.scripting.executeScript({
-//     target: { tabId: id, allFrames: true },
-//     files: ["index.js"],
-//   });
-
-//   console.log(`loading: ${url}`);
-// };
-
-// getCurrentTab = async () => {
-//   let queryOptions = { active: true };
-//   let [tab] = await chrome.tabs.query(queryOptions);
-//   return tab;
-// };
-
-// getCurrentTab().then((tab) => {
-//   getColorsScript(tab);
-// });
-
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ["index.js"],
   });
+
+  console.log("executing script");
 });
